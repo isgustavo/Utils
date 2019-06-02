21383610ab2a8df987b4077ab395e9d4f7fb6a63
isgustavo <hi.gustavo.oliveira@gmail.com>
2019-06-02 13:17:28 -0300

    Add health component

---
+++ Scripts/HealthComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    public int MaxHealth { get; private set; }
+    private int CurrentHealth { get; set; }
+
+    public Action<int> OnHealthChanged = delegate { };
+
+    private void Awake()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        CurrentHealth += Mathf.Clamp(CurrentHealth + amount, 0, MaxHealth);
+        OnHealthChanged.Invoke(CurrentHealth);
+    }
+}
