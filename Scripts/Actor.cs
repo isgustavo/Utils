3f71ccb9fa24c3417983fc942c76e3716c55b9de
isgustavo <hi.gustavo.oliveira@gmail.com>
2019-06-30 11:13:31 -0300

    Add Actor and health

---
+++ Scripts/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace odt.util
+{
+    public class Actor : MonoBehaviour
+    {
+        [SerializeField]
+        protected Health health;
+
+        protected StateMachine stateMachine;
+
+        protected void TakeDamage (int amount)
+        {
+            health.ChangeHealth(-amount);
+        }
+    }
+}
+
---
+++ Scripts/Health.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace odt.util
+{
+    [Serializable]
+    public class Health
+    {
+        [SerializeField]
+        private int InitHealth;
+        public int CurrentHealth { get; private set; }
+
+        public Action<int> OnHealthChanged = delegate { };
+
+        public void Init()
+        {
+            CurrentHealth = InitHealth;
+        }
+
+        public void ChangeHealth(int amount)
+        {
+            CurrentHealth = Mathf.Clamp(CurrentHealth + amount, 0, InitHealth);
+            OnHealthChanged.Invoke(CurrentHealth);
+        }
+    }
+}
+
